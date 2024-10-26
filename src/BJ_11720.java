383fef85094b90fca323e5069ba8abf1045fc713
WestSilver99 <seo606101@naver.com>
2024-10-26 16:33:30 +0900

    commit

---
+++ src/BJ_11720.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BJ_11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int num;
+        int sum = 0;
+
+        int[] arr = new int[N];
+
+        for(int i = 0 ; i<arr.length; i++){
+            arr[i] = num = sc.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
+
+
+
+    }
+}
